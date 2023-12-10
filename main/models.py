31c0ab50e447e603192de9a21aab30c9ae62d89b
seoluke7203 <seoluke7203@hotmail.com>
2023-12-10 05:24:03 -0800

    Django Full-stack Assignment

--- main/models.py
+++ main/models.py
@@ -6,7 +6,7 @@ class List(models.Model):
     lname = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
     email = models.CharField(max_length=100)
-    role = models.CharField(max_length=100)
+    role = models.CharField(max_length=10)
 
     def __str__(self):
         return self.email
